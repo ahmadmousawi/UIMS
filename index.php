6996ea206e78730a44ec43bc1bc6b4f477b9ecb1
ahmadmousawi <ahmad.mousawi@hotmail.com>
2015-11-29 01:22:51 +0300

    ahmadmousawi

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Ahmad
- * Date: 11/28/2015
- * Time: 11:47 PM
- */
-echo "ahmad";
--- xxx/xx.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Ahmad
- * Date: 11/29/2015
- * Time: 12:02 AM
- */
