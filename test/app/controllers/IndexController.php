fe15864a011429d149d7c72fca259024625bf756
FareedAlBender <fareedalbender@gmail.com>
2015-11-30 17:17:56 +0300

    testing views and controllers, creating index controller and view

---
+++ test/app/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fareed
+ * Date: 11/30/2015
+ * Time: 17:11
+ */
+use Phalcon\Mvc\Controller;
+class IndexController extends Controller
+{
+    public function indexAction()
+    {
+
+    }
+}
+?>
